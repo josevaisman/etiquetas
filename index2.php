fe9d9e2916df9a9691da1eb965fd8e5ebc98ff5a
Jose Vaisman <hellovaisman@gmail.com>
2016-09-29 00:11:20 -0300

    fondo

--- index2.php
+++ index2.php
@@ -1,25 +1,33 @@
 <?php
 
-$n = 2;
-$a = 3;
-$b = 5;
+$n = 1;
+$a = 4;
+$b = 6;
 $dx = 420;
 $dy = 200;
 $dm = 20;
 $dp = 40;
+$br = 2;
 
 $bgd = imagecreatetruecolor($a*($dx+2*$dm+$dp)+$dp, $b*($dy+2*$dm+$dp)+$dp);
-$fill = imagecreatetruecolor( $dx+2*$dm , $dy+2*$dm );
+$fill1 = imagecreatetruecolor( $dx+2*$dm , $dy+2*$dm );
+$fill2 = imagecreatetruecolor( $dx+2*$dm+2*$br , $dy+2*$dm+2*$br );
 
-$magenta = imagecolorallocate($bgd, 255, 0, 255);
-imagefill($bgd, 0, 0, $magenta);
+
+$white1 = imagecolorallocate($fill1, 255, 255, 255);
+imagefill($fill1, 0, 0, $white1);
+imagealphablending($fill1, false);
+imagesavealpha($fill1, true);
+
+$magenta = imagecolorallocate($fill2, 255, 0, 255);
+imagefill($fill2, 0, 0, $magenta);
+imagealphablending($fill2, false);
+imagesavealpha($fill2, true);
+
+$white2 = imagecolorallocate($bgd, 255, 255, 255);
+imagefill($bgd, 0, 0, $white2);
 imagealphablending($bgd, false);
 imagesavealpha($bgd, true);
-
-$white = imagecolorallocate($fill, 255, 255, 255);
-imagefill($fill, 0, 0, $white);
-imagealphablending($fill, false);
-imagesavealpha($fill, true);
 
 $nombre = "etiquetas_".$n;
 $files = glob('codigos/*.png', GLOB_BRACE);
@@ -29,7 +37,8 @@ foreach ($files as $file){
 		for($j = 0; $j < $b; ++$j){
 			if((int)substr($file, 16, 3) == (($n-1)*$a*$b)+$i*$b+$j+1){
 				$img = imagecreatefrompng($file);
-				imagecopymerge($bgd, $fill, $dp+$i*($dx+2*$dm+$dp), $dp+$j*($dy+2*$dm+$dp), 0, 0, $dx+2*$dm, $dy+2*$dm, 100);
+				imagecopymerge($bgd, $fill2, $dp+$i*($dx+2*$dm+$dp)-$br, $dp+$j*($dy+2*$dm+$dp)-$br, 0, 0, $dx+2*$dm+2*$br, $dy+2*$dm+2*$br, 100);
+				imagecopymerge($bgd, $fill1, $dp+$i*($dx+2*$dm+$dp), $dp+$j*($dy+2*$dm+$dp), 0, 0, $dx+2*$dm, $dy+2*$dm, 100);
 				imagecopymerge($bgd, $img, $dp+$i*($dx+2*$dm+$dp)+$dm, $dp+$j*($dy+2*$dm+$dp)+$dm, 0, 0, $dx, $dy, 100);
 			}
 		}
@@ -42,7 +51,8 @@ imagepng($bgd);
 
 imagedestroy($bgd);
 imagedestroy($magenta);
-imagedestroy($fill);
+imagedestroy($fill1);
+imagedestroy($fill2);
 imagedestroy($img);
 imagedestroy($while);
 
