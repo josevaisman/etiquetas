83e22c886b337a97dcfa1e5e147687d6c919fa46
josevaisman <hellovaisman@gmail.com>
2016-10-04 18:52:23 -0300

    Marco magenta

--- index2.php
+++ index2.php
@@ -1,8 +1,8 @@
 <?php
 
-$n = 2;
-$a = 3;
-$b = 3;
+$n = 1;
+$a = 4;
+$b = 9;
 $dx = 420;
 $dy = 200;
 $dm = 20;
@@ -10,22 +10,9 @@ $dp = 40;
 $br = 2;
 
 $bgd = imagecreatetruecolor($a*($dx+2*$dm+$dp)+$dp, $b*($dy+2*$dm+$dp)+$dp);
-$fill1 = imagecreatetruecolor( $dx+2*$dm , $dy+2*$dm );
-$fill2 = imagecreatetruecolor( $dx+2*$dm+2*$br , $dy+2*$dm+2*$br );
 
-
-$white1 = imagecolorallocate($fill1, 255, 255, 255);
-imagefill($fill1, 0, 0, $white1);
-imagealphablending($fill1, false);
-imagesavealpha($fill1, true);
-
-$magenta = imagecolorallocate($fill2, 255, 0, 255);
-imagefill($fill2, 0, 0, $magenta);
-imagealphablending($fill2, false);
-imagesavealpha($fill2, true);
-
-$white2 = imagecolorallocate($bgd, 255, 255, 255);
-imagefill($bgd, 0, 0, $white2);
+$while = imagecolorallocate($bgd, 255, 255, 255);
+imagefill($bgd, 0, 0, $while);
 imagealphablending($bgd, false);
 imagesavealpha($bgd, true);
 
@@ -37,8 +24,6 @@ foreach ($files as $file){
 		for($j = 0; $j < $b; ++$j){
 			if((int)substr($file, 16, 3) == (($n-1)*$a*$b)+$i*$b+$j+1){
 				$img = imagecreatefrompng($file);
-				imagecopymerge($bgd, $fill2, $dp+$i*($dx+2*$dm+$dp)-$br, $dp+$j*($dy+2*$dm+$dp)-$br, 0, 0, $dx+2*$dm+2*$br, $dy+2*$dm+2*$br, 100);
-				imagecopymerge($bgd, $fill1, $dp+$i*($dx+2*$dm+$dp), $dp+$j*($dy+2*$dm+$dp), 0, 0, $dx+2*$dm, $dy+2*$dm, 100);
 				imagecopymerge($bgd, $img, $dp+$i*($dx+2*$dm+$dp)+$dm, $dp+$j*($dy+2*$dm+$dp)+$dm, 0, 0, $dx, $dy, 100);
 			}
 		}
@@ -50,9 +35,6 @@ $imgfile = $nombre.'.jpeg';
 imagejpeg($bgd, $imgfile);
 
 imagedestroy($bgd);
-imagedestroy($magenta);
-imagedestroy($fill1);
-imagedestroy($fill2);
 imagedestroy($img);
 imagedestroy($while);
 
--- index3.php
+++ index3.php
@@ -1,7 +1,17 @@
 <?php
 
-require('fpdf181/fpdf.php');
-$n = 3;
+require('TCPDF/tcpdf.php');
+
+$n = 1;
+$a = 4;
+$b = 9;
+$dx = 420;
+$dy = 200;
+$dm = 20;
+$dp = 40;
+$br = 2;
+
+//$bgd = imagecreatetruecolor($a*($dx+2*$dm+$dp)+$dp, $b*($dy+2*$dm+$dp)+$dp);
 
 $nombre = "agregados/etiquetas_".$n;
 $nombrePdf = "imprimibles/etiquetas_imprimible_".$n;
@@ -9,9 +19,28 @@ $nombrePdf = "imprimibles/etiquetas_imprimible_".$n;
 $imgfile = $nombre.'.jpeg';
 $pdffile = $nombrePdf.'.pdf';
 
-$pdf = new FPDF('P','mm',array(560, 710));
-$pdf -> Image($imgfile, null, null, 200, 710);
-$pdf -> Output('F', $pdffile);
+$pdf = new TCPDF('P','mm',array(560, 710));
+$pdf->SetCreator(PDF_CREATOR);
+$pdf->SetAuthor('José Vaisman');
+$pdf->SetTitle('Codigos de Inventario');
+$pdf->setPrintHeader(false);
+$pdf->SetMargins(0, 0, 0);
+$pdf->SetAutoPageBreak(TRUE, 0);
+$pdf->AddPage();
+$pdf->setJPEGQuality(100);
+$pdf->Image($imgfile, 0, 0, 560, '', 'JPEG', '', '', true, 150, '', false, false, 0, false, false, false);
+
+for($j = 0; $j < $b; ++$j){
+	for($i = 0; $i < $a; ++$i){
+		//imagecopymerge($bgd, $img, $dp+$i*($dx+2*$dm+$dp)+$dm, $dp+$j*($dy+2*$dm+$dp)+$dm, 0, 0, $dx, $dy, 100);
+		$x = ($dp+$i*($dx+2*$dm+$dp))/($a*($dx+2*$dm+$dp)+$dp) * 560;
+		$y = ($dp+$j*($dy+2*$dm+$dp))/($a*($dx+2*$dm+$dp)+$dp) * 560;		
+		$pdf->SetXY(0, 0);
+		$pdf->ImageSVG($file='marco.svg', $x, $y, $w='', $h='', $link='', $align='', $palign='', $border=0, $fitonpage=false);
+	}
+}
+//$pdf->Output($pdffile, 'F');
+$pdf->Output($pdffile, 'I');
 
 ?>
 
